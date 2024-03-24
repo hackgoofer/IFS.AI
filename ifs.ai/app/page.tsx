c3215bd98e92a1081214e90e4f36d2e5c61655ef
Travis Cline <travis.cline@gmail.com>
2024-03-23 18:45:10 -0700

    hax: fix url for new session

--- ifs.ai/app/page.tsx
+++ ifs.ai/app/page.tsx
@@ -3,22 +3,26 @@ import { useState } from "react";
 import DIDVideoStream from "./DIDVideoStream";
 
 export default function Home() {
-  const [streamingClients, setStreamingClients] = useState([{
-    avatarUrl: "https://tmc.dev/agent-2.jpeg"
-  }]);
+  const [streamingClients, setStreamingClients] = useState([
+    {
+      avatarUrl: "https://storage.googleapis.com/tmc.dev/agent-2.jpeg",
+    },
+  ]);
 
   const createNewStreamingClient = () => {
-    setStreamingClients([...streamingClients, {}]);
+    setStreamingClients([
+      ...streamingClients,
+      {
+        avatarUrl: "https://storage.googleapis.com/tmc.dev/agent-3.jpeg",
+      },
+    ]);
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="content">
         {streamingClients.map((_, index) => (
-          <DIDVideoStream
-            key={index}
-            avatarUrl={streamingClients[index].avatarUrl}
-           />
+          <DIDVideoStream key={index} avatarUrl={streamingClients[index].avatarUrl} />
         ))}
         <button onClick={createNewStreamingClient}>Create New Streaming Client</button>
       </div>
